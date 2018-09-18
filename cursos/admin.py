fec2481c215c7b9213247ca8893403215608514a
unknown <cascosole@gmail.com>
2018-09-18 00:41:47 -0300

    lo ultimo, me fui a dormir

--- cursos/admin.py
+++ cursos/admin.py
@@ -2,3 +2,5 @@ from django.contrib import admin
 from .models import Curso
 # Register your models here.
 admin.site.register(Curso)
+admin.site.register(Materia)
+admin.site.register(Asistencia)
--- cursos/models.py
+++ cursos/models.py
@@ -3,11 +3,28 @@ from django.db import models
 # Create your models here.
 class Curso(models.Model):
 	"""docstring for Curso"""
-	materia=models.CharField(max_length=30)
+	materiaID=models.ForeignKey('cursos.Materia',blank=True,default="",on_delete=models.PROTECT)
 	modulo=models.IntegerField(blank=True)
-	fechaI=models.DateField(null=False, max_length=50)
-	fechaF=models.DateField(null=False, max_length=50)
 	profesorID=models.ForeignKey('personas.Profesor', blank=True, default="", on_delete=models.PROTECT)
-	
+	mes=models.CharField(max_length=10)
+	anio=models.IntegerField(blank=True)
+	estado=models.CharField(max_length=15)
 	def __str__(self):
-		return self.materia
+		return self.mes + self.estado
+
+class Materia(models.Model):
+	nombre=models.CharField(max_length=50)
+	tipo=models.CharField(max_length=20)
+	def __str__(self):
+		return self.mes + self.estado
+
+
+
+class Asistencia(models.Model):
+	"""docstring for Asistencia"""
+	cursoID=models.ForeignKey('cursos.Curso',blank=True,default="",on_delete=models.PROTECT)
+	alumnoID=models.ForeignKey('personas.Alumno',blank=True,default="",on_delete=models.PROTECT)
+	fecha=models.DateTimeField(auto_now_add=True)
+	
+		
+
