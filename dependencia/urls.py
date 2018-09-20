380405c8be7ec6f39ff955b60eda687366960fa8
unknown <cascosole@gmail.com>
2018-09-19 21:52:26 -0300

    no hice mucho, estoy aprendiendo XD, voy a tratar de meterle pata

--- dependencia/urls.py
+++ dependencia/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
-from . import views
+from . import views 
 
 urlpatterns = [
-url(r'^$', views.dependencias, name="dependencias")
+url(r'^$', views.indexDependencia, name="index_dependencia"),
+url(r'^mostrar/', views.dependencias, name="dependencias"),
+
+
 ]
--- dependencia/views.py
+++ dependencia/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from dependencia.models import *
 # Create your views here.
+
+def indexDependencia(request):
+    return HttpResponse("pag principal...En construccion")
 
 def dependencias(request):
     return render(request, 'dependencia/dependencias.html', {
         'dependencia': Dependencia.objects.all(),
         
     })
+    
