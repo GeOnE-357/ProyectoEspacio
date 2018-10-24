168eb0185a89ca43b3bbe1869bb4c8699dbf2b24
GeOnE-357 <emedarias@gmail.com>
2018-10-24 17:03:33 -0300

    Arreglado el boton de crear en todos los index.

--- personas/views.py
+++ personas/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from personas.models import *
+from espacio.forms import UsuarioForm
 from .forms import profesorForm, alumnoForm
 
 def personasListar(request, tipo):
