d4f5688a93cfddd703ea153c4457338c435b2e8b
GeOnE-357 <emedarias@gmail.com>
2018-10-25 22:27:40 -0300

    MegaCUAK!!!

--- dependencia/views.py
+++ dependencia/views.py
@@ -37,10 +37,11 @@ def crearAula(request):
 
 def detalleCrear(request):
 
-	det=DetalleAula()
+	
 	a=Aula.objects.latest('id')
 	for d in Dia.objects.all():
 		for h in Horario.objects.all():
+			det=DetalleAula()
 			det.aulaId=a
 			det.diaId=d
 			det.estado="disponible"
