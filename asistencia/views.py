2ed8c6aaa7c1a202ed0327bb07e97094e32b76df
unknown <cascosole@gmail.com>
2018-12-03 10:04:41 -0300

    asist

--- asistencia/views.py
+++ asistencia/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render, redirect
 from .models import Asistencia
 from .forms import AsistenciaForm
 
-def index(request):
-    return render(request, 'asistencia/index.html', {
-        'cursos': cursos.objects.filter(profesorId=id),
+def index(request,usuario):
+	'profe': profesor.objects.filter(dni=usuario)
+	'cur':cursos.objects.filter(profesorId=profe.id,estado='vigente')
+    return render(request, 'asistencia/index.html',
     })
-#deberia recibir el id del profesor que 
+#deberia recibir el user del profesor que 
 #loguee y traer las inscripciones de sus cursos
-def listar_asist(request):
-	pass
+def listarCurso(request):
+	return render(request, 'asistencia/detalleCurso',{
+		'inscriptos': Inscripcion.objects.filter(estado='vigente'),
+		})
