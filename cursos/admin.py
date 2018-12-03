a63728437f3304375ab6c7e1bad7d642d988a0dc
GeOnE-357 <emedarias@gmail.com>
2018-12-03 11:36:26 -0300

    Mejorada la barra de navegacion.

--- cursos/admin.py
+++ cursos/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import Curso, Materia, Inscripcion, Mes
+from .models import Curso, Materia, Inscripcion
 # Register your models here.
 admin.site.register(Curso)
 admin.site.register(Materia)
 admin.site.register(Inscripcion)
-admin.site.register(Mes)
