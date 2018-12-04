e19b409f642919d64467116fbccd62db6a381ad5
unknown <cascosole@gmail.com>
2018-12-04 08:37:24 -0300

    conflicto

---
+++ cursos/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Curso, Materia, Inscripcion
+# Register your models here.
+
+admin.site.register(Curso)
+admin.site.register(Materia)
+admin.site.register(Inscripcion)
+
---
+++ cursos/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_list_or_404, get_object_or_404
+from .models import Curso, Materia, Inscripcion
+from personas.models import Profesor
+from .forms import CursoForm, MateriaForm, InscripcionForm
+from .filters import CursoFilter
+
+def cursosListar(request):
+	cursos= Curso.objects.all()
+	materias = Materia.objects.all()
+	profes = Profesor.objects.all()
+	filtro = CursoFilter(request.GET, queryset=cursos)
+	return render(request, 'cursos/index.html', {'filtro':filtro, 'materias':materias, 'profes':profes} )
+
+
+def cursosCrear(request):
+	if request.method=="POST":
+		form=CursoForm(request.POST or none)
+		if form.is_valid():
+			instacia= form.save(commit=False)
+			instacia.save()
+			return redirect('cursos-index')
+	else:
+		form=CursoForm()
+	return render(request, 'cursos/crear.html', {'form':form})
+
+
+def materiasListar(request):
+	return render(request, 'materias/index.html', {'materias': Materia.objects.all()})
+
+
+def materiasCrear(request):
+	if request.method=="POST":
+		form=MateriaForm(request.POST or none)
+		if form.is_valid():
+			instacia= form.save(commit=False)
+			instacia.save()
+			return redirect('materias-index')
+	else:
+		form=MateriaForm()
+	return render(request, 'materias/crear.html', {'form':form})
+
+
+def inscripcionCrear(request, id):
+	if request.method=="POST":
+		form=InscripcionForm(request.POST or none)
+		if form.is_valid():
+			instancia= form.save(commit=False)
+			instancia.save()
+	else:
+		form=InscripcionForm()
+	return render(request, 'cursos/inscripcion.html', {'form':form})
