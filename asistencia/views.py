97d0f0674324bdf359bfbcb9f423e1ffeea2580b
GeOnE-357 <emedarias@gmail.com>
2018-12-07 08:55:29 -0300

    Arreglado el problema de ver los cursos.

--- asistencia/views.py
+++ asistencia/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render, redirect, get_list_or_404, get_object_or_404
 from .models import Asistencia
+from personas.models import Profesor
+from cursos.models import Curso
 from .forms import AsistenciaForm
 
 def index(request,usuario):
-	profe: profesor.objects.filter(dni=usuario)
-	cur:cursos.objects.filter(profesorId=profe.id,estado='vigente')
-	return render(request, 'asistencia/index.html')
-#deberia recibir el user del profesor que 
-#loguee y traer las inscripciones de sus cursos
+	dni=int(usuario)
+	persona = get_object_or_404(Profesor, dni=dni)
+	curso=Curso.objects.filter(profesorID=persona.id)
+	return render(request, 'asistencia/index.html',{'cursos':curso})
+
 def listarCurso(request):
 	return render(request, 'asistencia/detalleCurso',{
-		'inscriptos': Inscripcion.objects.filter(estado='vigente'),
-		})
+		'inscriptos': Inscripcion.objects.filter(estado='vigente')})
