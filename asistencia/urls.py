3ab6092d0955e55969dc97cbfd423149a2f7df53
GeOnE-357 <emedarias@gmail.com>
2018-12-21 16:21:14 -0300

    Asistencia creado!

--- asistencia/urls.py
+++ asistencia/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
 url(r'^Mostrar/MisCursos/(?P<usuario>\w+)/$', views.index, name="asistencia-index"),
 url(r'^Mostrar/MisCursos/Inscriptos/(?P<curso>\d+)$', views.listarAlumnoCurso, name="alumno-index"),
 url(r'^Mostrar/MisCursos/Asistencia/(?P<curso>\d+)$', views.listarAsistenciaCurso, name="alumno-asistencia"),
+url(r'^Mostrar/MisCursos/Asistencia/Realizada/$', views.asistenciaCrear, name="asistencia-crear"),
 ]
