41151e363a70c82f5c05d1cc68977bc749980507
GeOnE-357 <emedarias@gmail.com>
2019-02-05 14:01:44 -0300

    Agregado el tiempo de carga con javascript.

---
+++ static/js/cargando.js
@@ -0,0 +1,7 @@
+var bot = document.getElementById('cargar');
+bot.onclick=cargando;
+function cargando()
+	{
+		window.scrollTo(0,0);
+		document.getElementsByClassName("contenedor")[0].style.display="block";		
+	}
