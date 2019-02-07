1dc3ea9fb2dc0acf45f7abb9686a43d693d5aee3
GeOnE-357 <emedarias@gmail.com>
2019-02-06 22:06:49 -0300

    Arreglados todos los titulos y opciones de los formularios.

--- cursos/forms.py
+++ cursos/forms.py
@@ -3,23 +3,27 @@ from .models import Curso, Materia,Inscripcion
 from personas.models import Profesor, Alumno
 
 class CursoForm(forms.ModelForm):
-	anio = forms.IntegerField(required=True, label='Año:')
-	fInicio=forms.DateField(widget = forms.SelectDateWidget(), label='Fecha de Inicio:')
-	fFin=forms.DateField(widget = forms.SelectDateWidget(), label='Fecha de Finalizacion:')
-	cantClases=forms.IntegerField(required=True, label='Cantidad de Clases:')
+	ESTADO=(("Próximo", "Próximo"),("En curso", "En curso"),("Finalizado", "Finalizado"),)
+
 	materiaID=forms.ModelChoiceField(queryset=Materia.objects.all(), label="Materia:", required=True)
 	profesorID=forms.ModelChoiceField(queryset=Profesor.objects.all(), label="Profesor:", required=True)
+	cantClases=forms.IntegerField(required=True, label='Cantidad de Clases:')
+	fInicio=forms.DateField(widget = forms.SelectDateWidget(), label='Fecha de Inicio:')
+	fFin=forms.DateField(widget = forms.SelectDateWidget(), label='Fecha de Finalizacion:')
+	anio = forms.IntegerField(required=True, label='Año:')
+	estado=forms.ChoiceField(choices=ESTADO , label="Estado:", widget=forms.Select(), required=True)
+
 	class Meta:
 		model = Curso
 		fields = ('materiaID', 'modulo', 'profesorID', 'cantClases', 'fInicio', 'fFin', 'anio', 'estado')
 
 class MateriaForm(forms.ModelForm):
 	
-	TIPO_CHOICES = (
+	TIPO= (
     ("Tecnológico", "Tecnológico"),
     ("Oficio Tradicional", "Oficio Tradicional"),)
 
-	tipo = forms.ChoiceField(choices = TIPO_CHOICES, label="Tipo:", widget=forms.Select(), required=True)
+	tipo = forms.ChoiceField(choices = TIPO, label="Tipo:", widget=forms.Select(), required=True)
 	nombre = forms.CharField(required=True)
 	class Meta:
 		model = Materia
