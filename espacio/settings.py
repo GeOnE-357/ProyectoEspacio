231bae4490d94f8eb7a3ec00afaf0356881f1c66
emedarias@gmail.com <gestionedlt@polaris.dir.riogrande.gob.ar>
2019-03-28 18:30:28 -0300

    archivo conf

--- espacio/settings.py
+++ espacio/settings.py
@@ -145,6 +145,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = 'static'
 
 MEDIA_URL = '/media/'
 MEDIAFILES_DIRS=[os.path.join(BASE_DIR, "media")]
+MEDIA_ROOT = 'media'
