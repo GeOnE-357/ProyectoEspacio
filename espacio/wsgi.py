b2fe6ba7b1b91f8a6fd027c5455bda52a9e2457f
GeOnE-357 <emedarias@gmail.com>
2019-04-03 17:47:14 -0300

    mas cambios para mod_wsgi

--- espacio/wsgi.py
+++ espacio/wsgi.py
@@ -14,4 +14,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ['DJANGO_SETTINGS_MODULE'] = '{{ espacio.settings}}'
 #os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'espacio.settings')
 
+os.environ.setdefault('LANG', 'en_US.UTF-8')
+os.environ.setdefault('LC_ALL', 'en_US.UTF-8')
+
 application = get_wsgi_application()
